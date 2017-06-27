5abcaf79539c95ac174a31e52c4a201b242bb58b
shreyas-a <shreyas_a24@yahoo.co.in>
2017-06-27 10:05:34 +0530

    count from main to popup

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-console.log('Domain: ' + document.domain);
+var count = 0;
 
-document.addEventListener('click', function (event) {
-  console.log('Clicked');
-})
+document.addEventListener("click", function(event) {
+  count += 1;
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  sendResponse({ count: count });
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,5 @@
-chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, function(tabs) {
-    alert('Tabs: ' + JSON.stringify(tabs))
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, { details: "count" }, function(response) {
+    document.getElementById("count").innerText = response.count;
   });
+});
