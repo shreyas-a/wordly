9a46b1d211dec58affccbbf8095db8a7ad093411
shreyas-a <shreyas.a24@gmail.com>
2017-07-13 16:48:14 +0530

    corrected click handling

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
 // Listening all messages
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.word) {
+    // Double Click
     var code = [
       'var d = document.createElement("div");',
+      'd.setAttribute("id","wordly")',
       "d.innerHTML='" + request.word + "'",
       'd.setAttribute("style", "' +
         "border: 1px solid #999;" +
-        "top: 1px;" +
-        "left: 1px;" +
+        "top: " +
+        request.y +
+        "px;" +
+        "left: " +
+        request.x +
+        "px;" +
         "border-radius: 4px;" +
         "box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);" +
         "color: #222;" +
@@ -18,9 +24,12 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
         'width: 300px;");',
       "document.body.appendChild(d);"
     ].join("\n");
-
-    chrome.tabs.executeScript(null, {
-      code: code
-    });
+  } else {
+    // Single Click
+    var code = 'document.getElementById("wordly").outerHTML="";';
   }
+
+  chrome.tabs.executeScript(null, {
+    code: code
+  });
 });
--- content-script.js
+++ content-script.js
@@ -1,18 +1,48 @@
-var selectedNode;
+var word, time;
+clickCount = 0;
 
-document.addEventListener("mouseup", function(event) {
+function sendMessage(event) {
+  // Sending message to background
+  chrome.runtime.sendMessage({
+    word: word,
+    x: event.clientX,
+    y: event.clientY
+  });
+}
+
+function singleClick(event) {
+  word = null;
+  sendMessage(event);
+}
+
+function doubleClick(event) {
   var range = window.getSelection().getRangeAt(0);
   selectedNode = range.cloneContents();
 
   if (selectedNode.textContent && selectedNode.textContent.length) {
-    // Ssending message to background
-    chrome.runtime.sendMessage({
-      word: selectedNode.textContent
-    });
+    word = selectedNode.textContent.split(" ")[0];
+  } else {
+    word = null;
+  }
+  sendMessage(event);
+}
+
+document.addEventListener("click", function(event) {
+  debugger;
+  clickCount++;
+  if (clickCount === 1) {
+    singleClickTimer = setTimeout(function() {
+      clickCount = 0;
+      singleClick(event);
+    }, 400);
+  } else if (clickCount === 2) {
+    clearTimeout(singleClickTimer);
+    clickCount = 0;
+    doubleClick(event);
   }
 });
 
 // Listening messages from extention (popup) or background
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  sendResponse({ selectedText: selectedNode.textContent });
+  sendResponse({ selectedText: word });
 });
